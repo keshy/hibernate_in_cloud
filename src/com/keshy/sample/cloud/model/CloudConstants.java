a970d03dee344895c66f8de3883bb721838d5a30
keshy <keshi8086@gmail.com>
2012-10-02 18:18:16 -0300

    Update src/com/keshy/sample/cloud/model/CloudConstants.java

--- src/com/keshy/sample/cloud/model/CloudConstants.java
+++ src/com/keshy/sample/cloud/model/CloudConstants.java
@@ -4,9 +4,9 @@ public class CloudConstants {
    
    public static final String S3_BUCKET = "test.aws.bucket";
    
-   public static final String AWS_PUBLIC_KEY = "AKIAJZRZONX5Q2EBU37Q";
+   public static final String AWS_PUBLIC_KEY = "*****";
    
-   public static final String AWS_PRIVATE_KEY = "86DN77Olg6vwXN1tc62ZKDAGMKrWf/4V9YxgOLoP";
+   public static final String AWS_PRIVATE_KEY = "*********";
    
    public static final String NUMBER = "number";
    
